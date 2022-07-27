37975e10404590cb8c107a5f4f0e86d672a1a045
Soutrik Bandyopadhyay <soutrik.band@gmail.com>
2022-07-27 14:31:00 +0530

    Courses

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 import json
+
 import pandas as pd
 
-with open("data/faculty.json") as f:
-    data = json.load(f)
 
-facultyFields = [
-        'name',
-        'position',
-        'education',
-        'research',
-        'room',
-        'email',
-        'homepage',
-        'avatar'
-        ]
-
-mega = []
-for i in data:
+def processPGCourses():
     ans = []
-    for field in facultyFields:
-        ans.append(i[field])
-    mega.append(ans)
+    columns = ["id", "name", "credit", "description", "core"]
+    data = pd.read_csv("csvData/pgcourses.csv")
+    numCourses = data.shape[0]
+
+    for i in range(numCourses):
+        unit = {}
+        for j in columns:
+            unit[j] = data[j][i]
+        ans.append(unit)
+    result = json.dumps(ans, indent=4)
+    with open("data/pgcourses.json", "w") as f:
+        f.write(result)
 
 
+def processPhDCourses():
+    ans = []
+    columns = ["id", "name", "credit", "description"]
+    data = pd.read_csv("csvData/phdcourses.csv")
+    numCourses = data.shape[0]
+
+    for i in range(numCourses):
+        unit = {}
+        for j in columns:
+            unit[j] = data[j][i]
+        ans.append(unit)
+    result = json.dumps(ans, indent=4)
+    with open("data/phdcourses.json", "w") as f:
+        f.write(result)
 
 
+def processUGCourses():
+    ans = []
+    columns = ["id", "name", "credit", "description", "core"]
+    data = pd.read_csv("csvData/ugcourses.csv")
+    numCourses = data.shape[0]
 
+    for i in range(numCourses):
+        unit = {}
+        for j in columns:
+            unit[j] = data[j][i]
+        ans.append(unit)
+    result = json.dumps(ans, indent=4)
+    with open("data/ugcourses.json", "w") as f:
+        f.write(result)
+
+
+if __name__ == "__main__":
+    processPGCourses()
+    processPhDCourses()
+    processUGCourses()
