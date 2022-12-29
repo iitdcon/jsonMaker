cb24e433082c38dbe3abc7737bd67dcf4a145e1b
Soutrik Bandyopadhyay <soutrik.band@gmail.com>
2022-12-29 13:57:51 +0530

    Publications added

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
+"""Create json from Excel data."""
 import json
+import os
+
 import pandas as pd
 
-with open("data/faculty.json") as f:
-    data = json.load(f)
+datas = []
+for i in os.listdir("ExcelData/publications"):
+    data = pd.read_excel(f"ExcelData/publications/{i}")
+    datas.append(data)
 
-facultyFields = [
-        'name',
-        'position',
-        'education',
-        'research',
-        'room',
-        'email',
-        'homepage',
-        'avatar'
-        ]
-
-mega = []
-for i in data:
-    ans = []
-    for field in facultyFields:
-        ans.append(i[field])
-    mega.append(ans)
-
-
-
-
-
+finalData = pd.concat(datas)
+finalData.columns = ["year", "citation", "type", "doi"]
+json_string = finalData.to_json(orient="records")
+parsed = json.loads(json_string)
+with open("data/publications.json", "w") as f:
+    f.write(json.dumps(parsed, indent=4))
